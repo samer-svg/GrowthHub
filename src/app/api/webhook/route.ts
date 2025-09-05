d9fe0a197272e9a3d640f5c37f23ac2cdb8df2fd
Samer <sameralyaghn547@gmail.com>
2025-09-05 17:28:29 +0300

    deleting the version of Stripe

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,9 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 import { prisma } from "@/lib/prisma";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-07-30.basil",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 // App Router does NOT use default export
 export const POST = async (req: NextRequest) => {
